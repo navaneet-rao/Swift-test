140f0132f359f76dffb408cb7271e544bc5b7f29
Navaneet r rao <76439641+navaneet-rao@users.noreply.github.com>
2022-12-24 19:35:44 +0530

    changed app js

--- scr/app.js
+++ scr/app.js
@@ -1,14 +1,14 @@
-// const express = require("express");
-// const app = express();
+const express = require("express");
+const app = express();
 
-// app.get("/", function (req, res) {
-//   res.send("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n");
-// });
+app.get("/", function (req, res) {
+  res.send("under develop section \n");
+});
 
-// app.listen(8080, function () {
-//   console.log("Example app listening on port 8080!");
-// });
+app.listen(8080, function () {
+  console.log("app listening on port localhost:8080");
+});
 
-// module.exports = app;
+module.exports = app;
 
 console.log("Hell world \n");
